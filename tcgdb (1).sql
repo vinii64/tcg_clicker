54b5cb17997a51b3c5da0b1b0e48fd119efe824c
joan2262 <joangg1111@gmail.com>
2026-05-20 20:44:10 -0300

    Add files via upload
    base de datos

---
+++ tcgdb (1).sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-05-2026 a las 01:43:01
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tcgdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cartas`
+--
+
+CREATE TABLE `cartas` (
+  `id` int(11) NOT NULL,
+  `img` varchar(100) NOT NULL,
+  `rareza` varchar(50) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cartas`
+--
+
+INSERT INTO `cartas` (`id`, `img`, `rareza`, `tipo`) VALUES
+(1, 'cartas/0.png', 'comun', 'normal'),
+(2, 'cartas/1.png', 'comun', 'normal'),
+(3, 'cartas/2.png', 'comun', 'normal'),
+(4, 'cartas/3.png', 'comun', 'normal'),
+(5, 'cartas/4.png', 'comun', 'normal');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `id_usuario` int(11) NOT NULL,
+  `id_carta` int(11) NOT NULL,
+  `cantidad` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `contrasenia` varchar(50) NOT NULL,
+  `puntos` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `usuario`, `admin`, `contrasenia`, `puntos`) VALUES
+(37, 'vinzera@gmail.com', 'vinzera64', 0, 'fc4e20a9807491b3614409fef7668b53', 7),
+(38, 'sofi@gmail.com', 'sofiBB', 0, 'f1e654d20b3aad4183a01c0cf88f7c66', 1995767),
+(39, 'agomez@eest3necochea.edu.ar', 'Ale', 0, '81dc9bdb52d04dc20036dbd8313ed055', 17),
+(40, 'vinzera', 'vinzera', 0, 'd88518acbcc3d08d1f18da62f9bb26ec', 3290),
+(41, 'sofia@gmail.com', 'sofia', 0, '17da1ae431f965d839ec8eb93087fb2b', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cartas`
+--
+ALTER TABLE `cartas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD KEY `cartas_id` (`id_carta`),
+  ADD KEY `usuarios_id` (`id_usuario`) USING BTREE;
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cartas`
+--
+ALTER TABLE `cartas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `cartas_id` FOREIGN KEY (`id_carta`) REFERENCES `cartas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `usuario_id` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
